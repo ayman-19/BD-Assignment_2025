6fa5137bf11c2c57382d3ff90e3a673a2d40067b
ayman-19 <aymanroshdy909@email.com>
2025-10-29 16:31:19 +0300

    commit

--- BD-Assignment_2025/BackgroundJobs/ExpiredBlockJob.cs
+++ BD-Assignment_2025/BackgroundJobs/ExpiredBlockJob.cs
@@ -1,17 +1,10 @@
 ﻿namespace BD_Assignment_2025.BackgroundJobs;
 
-public class ExpiredBlockJob : IJob
+public class ExpiredBlockJob(IServiceScopeFactory scopeFactory) : IJob
 {
-    private readonly IServiceScopeFactory _scopeFactory;
-
-    public ExpiredBlockJob(IServiceScopeFactory scopeFactory)
-    {
-        _scopeFactory = scopeFactory;
-    }
-
     public async Task Execute(IJobExecutionContext context)
     {
-        using var scope = _scopeFactory.CreateScope();
+        using var scope = scopeFactory.CreateScope();
         var repository = scope.ServiceProvider.GetRequiredService<IBlockedCountryRepository>();
         await repository.RemoveExpiredBlocks();
     }
--- BD-Assignment_2025/Entities/BlockCountry.cs
+++ BD-Assignment_2025/Entities/BlockCountry.cs
@@ -1,3 +1,3 @@
 ﻿namespace BD_Assignment_2025.Entities;
 
-public sealed record BlockCountry(string Code, DateTime? Duration);
+public sealed record BlockCountry(string Code, DateTime? Duration = null);
